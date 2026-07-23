2af6ec796da6614de39c19d7fd9dd4ff91e434cc
Alejandro <amondragonflrs@gmail.com>
2026-07-23 13:06:18 -0500

    Add basic error handling with die()

--- main.c
+++ main.c
@@ -1,19 +1,28 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <termios.h>
-
+#include <errno.h>
 #include <ctype.h>
 #include <stdio.h>
 
 struct termios orig_termios;
 
+void die(const char *s){
+    perror(s);
+    exit(1);
+}
+
 void disableRawMode(void){
     write(STDOUT_FILENO, "\x1b[?1049l", 8);   // exit alternate screen
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1){
+        die("tcsetattr");
+    }
 }
 
 void enableRawMode(void){
-    tcgetattr(STDIN_FILENO, &orig_termios);
+    if (tcgetattr(STDIN_FILENO, &orig_termios) == -1){
+        die("tcgetattr");
+    }
     atexit(disableRawMode);
 
     write(STDOUT_FILENO, "\x1b[?1049h", 8); // enter alternate screen
@@ -23,19 +32,29 @@ void enableRawMode(void){
     raw.c_oflag &= ~(OPOST);
     raw.c_cflag |= (CS8);
     raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+
+    raw.c_cc[VMIN] = 0;
+    raw.c_cc[VTIME] = 1;
+
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1){
+        die("tcsetattr");
+    }
 }
 
 int main (void){
     enableRawMode();
 
-    char c; 
-    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
+    while (1) {
+        char c = '\0';
+        if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN){
+            die("read");
+        } 
         if (iscntrl(c)) {
             printf("%d\r\n", c);
         } else {
             printf("%d ('%c')\r\n", c, c);
         }
+        if (c == 'q') { break ; }
     }
     return 0;
 }
